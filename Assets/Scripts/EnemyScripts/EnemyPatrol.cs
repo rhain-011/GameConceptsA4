5a3c7ebfdc5666be19f132e1a91ddea3e5873700
Rhainel <rhainel@users.noreply.github.com>
2019-01-18 16:59:26 -0500

    working set enemy patrol

--- Assets/Scripts/EnemyScripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyScripts/EnemyPatrol.cs
@@ -13,11 +13,11 @@ public class EnemyPatrol : MonoBehaviour
 
     // wait time at each point
     [SerializeField]
-    float totalWaitTime;
+    float totalWaitTime = 2.0f;
 
     // probabaility that the agent will change course
     [SerializeField]
-    float changeDestinationProb;
+    float changeDestinationProb = 0.1f;
 
     [SerializeField]
     List<Waypoint> patrolPoints;
@@ -52,12 +52,72 @@ public class EnemyPatrol : MonoBehaviour
         }
     }
 
+
+    private void Update()
+    {
+        if (isTravelling && navMeshAgent.remainingDistance <= 1.0f)
+        {
+            isTravelling = false;
+
+            if (isPatrolWaiting)
+            {
+                isWaiting = true;
+                waitTimer = 0.0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                Setdestination();
+            }
+        }
+
+
+        if (isWaiting)
+        {
+            waitTimer += Time.deltaTime;
+
+            if (waitTimer >= totalWaitTime)
+            {
+                isWaiting = false;
+
+                ChangePatrolPoint();
+                Setdestination();
+            }
+        }
+    }
+
+    // sets the destination of the agent to go to
     private void Setdestination()
     {
-        if(waypoint != null)
+        if (patrolPoints != null)
         {
-            Vector3 targetDestination = waypoint.transform.position;
+            Vector3 targetDestination = patrolPoints[currentPatrolIndex].transform.position;
             navMeshAgent.SetDestination(targetDestination);
+            isTravelling = true;
+        }
+    }
+
+
+    // select a new patrol point in the list but a smmal probabability for the agent to move forward or backwards.
+    private void ChangePatrolPoint()
+    {
+        if(UnityEngine.Random.Range(0.0f, 1.0f) <= changeDestinationProb)
+        {
+            patrolForward = !patrolForward;
+        }
+
+        // checks if currentpatrolindex is greater than the amount patrol points in the list
+        // resets  back to zero
+        if (patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Count;
+        }
+        else
+        {
+            if(--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Count - 1;
+            }
         }
     }
 }
