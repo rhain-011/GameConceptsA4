ba8831781b1afdd2941103d972a477d2b61382d6
Rhainel <rhainel@users.noreply.github.com>
2019-01-18 17:26:52 -0500

    working patrol on main scene

--- Assets/Scripts/FSM_Scripts/Patrol.cs
+++ Assets/Scripts/FSM_Scripts/Patrol.cs
@@ -5,44 +5,87 @@ using UnityEngine;
 public class Patrol : NPCBaseFSM
 {
 
-    GameObject[] waypoints; // array of waypoints
-    int currentWaypoint; // waypoint going to
+    // dictates if agent waits on each waypoint
+    [SerializeField]
+    bool isPatrolWaiting;
+
+    // wait time at each point
+    [SerializeField]
+    float totalWaitTime = 2.0f;
+
+    // probabaility that the agent will change course
+    [SerializeField]
+    float changeDestinationProb = 0.1f;
+
+    [SerializeField]
+    //List<Waypoint> patrolPoints;
+    GameObject[] patrolPoints; // array of waypoints
+
+    int currentPatrolIndex;
+    bool isTravelling;
+    bool isWaiting;
+    bool patrolForward;
+    float waitTimer;
 
     private void Awake()
     {
-        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        patrolPoints = GameObject.FindGameObjectsWithTag("waypoint");
     }
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
-        currentWaypoint = Random.Range(0, waypoints.Length); // set destination into a a random waypoint
+        
+
+        // checks if gameobject has a NavMeshAgent attached to it
+        if (agent == null)
+        {
+            Debug.LogError("The Nav Mesh Agent component is not attached to " + NPC.name);
+        }
+        else
+        {
+            if (patrolPoints != null && patrolPoints.Length >= 2)
+            {
+                currentPatrolIndex = 0;
+                Setdestination();
+            }
+        }
 
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // returns if theres no waypoints
-        if (waypoints.Length == 0) return;
-
-        // distance of NPC in relation to current waypoint
-        if (Vector3.Distance(waypoints[currentWaypoint].transform.position, NPC.transform.position) < accuracy)
+        if (isTravelling && agent.remainingDistance <= 1.0f)
         {
-            currentWaypoint++;
-            if (currentWaypoint >= waypoints.Length)
+            isTravelling = false;
+
+            if (isPatrolWaiting)
             {
-                currentWaypoint = 0;
+                isWaiting = true;
+                waitTimer = 0.0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                Setdestination();
             }
         }
-        
-        //// rotate and move towards next waypoint
-        //// direction where NPC is going
-        //var direction = waypoints[currentWaypoint].transform.position - NPC.transform.position; 
-        //// apply a rotation to the NPC to face the desired direction
-        //NPC.transform.rotation = Quaternion.Slerp(NPC.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
-        //NPC.transform.Translate(0, 0, Time.deltaTime * speed);
+
+
+        if (isWaiting)
+        {
+            waitTimer += Time.deltaTime;
+
+            if (waitTimer >= totalWaitTime)
+            {
+                isWaiting = false;
+
+                ChangePatrolPoint();
+                Setdestination();
+            }
+        }
 
     }
 
@@ -51,5 +94,38 @@ public class Patrol : NPCBaseFSM
     {
 
     }
-    
+
+    private void Setdestination()
+    {
+        if (patrolPoints != null)
+        {
+            Vector3 targetDestination = patrolPoints[currentPatrolIndex].transform.position;
+            agent.SetDestination(targetDestination);
+            isTravelling = true;
+        }
+    }
+
+
+    // select a new patrol point in the list but a smmal probabability for the agent to move forward or backwards.
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0.0f, 1.0f) <= changeDestinationProb)
+        {
+            patrolForward = !patrolForward;
+        }
+
+        // checks if currentpatrolindex is greater than the amount patrol points in the list
+        // resets  back to zero
+        if (patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+        }
+        else
+        {
+            if (--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Length - 1;
+            }
+        }
+    }
 }
